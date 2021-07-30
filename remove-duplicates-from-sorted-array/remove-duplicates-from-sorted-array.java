3b758a892d08986b9060ab9414e86e8e3b1890df
Lakshmi Tanuja Tammireddy <tanujatammireddy@gmail.com>
2021-07-30 01:05:16 -0500

    Update remove-duplicates-from-sorted-array.java

--- remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -1,34 +1,14 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int count =0;
-        if(nums.length == 1) return 1;
-        for(int index =0; index<nums.length;index++){
-            int i = index;
-            int j= index;
-            boolean flag = false;
-            if((i+1 < nums.length && nums[i] == nums[i+1])){
-            while(i+1 < nums.length && nums[i] == nums[i+1]){
-                i++;
-                flag = true;
-            }
-            if(flag){
-                //System.out.println("Reached Here");
-                while(i<nums.length-1){
-                    i++;
-                    j++;
-                    nums[j] = nums[i];
-                }
-            }  
-            }
-            
-            
+    if (nums.length == 0) return 0;
+    int i = 0;
+    for (int j = 1; j < nums.length; j++) {
+        
+        if (nums[j] != nums[i]) {
+            i++;
+           nums[i] = nums[j];
         }
-         for(int k =0; k<nums.length;k++){
-             if(k+1 < nums.length && nums[k] < nums[k+1]){
-                 count++;
-             }
-             else break;
-         }
-        return count+1;
     }
+    return i + 1;
+}
 }

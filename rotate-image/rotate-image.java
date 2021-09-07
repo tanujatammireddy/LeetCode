ee834607308c6e5bd479d65f58fc29a85030d2e6
Lakshmi Tanuja Tammireddy <tanujatammireddy@gmail.com>
2021-09-07 13:55:25 -0500

    Time: 0 ms (100.00%), Space: 39.1 MB (67.59%) - LeetHub

--- rotate-image/rotate-image.java
+++ rotate-image/rotate-image.java
@@ -2,30 +2,20 @@ class Solution {
     public void rotate(int[][] matrix) {
         
         int n = matrix.length;
-
-        int col = n-1;
-        
-        for(int i = 0 ; i < (n+1)/2; i++){
-            
-        for(int j = 0 ; j < n/2; j++){
-            
-           // System.out.println(" matrix[i][j]"+ matrix[i][j]);
-            
-            int temp = matrix[col-j][i];
-
-            matrix[col-j][i] = matrix[col-i][col-j];
-            matrix[col-i][col-j] = matrix[j][col-i];
-            matrix[j][col-i] = matrix[i][j];
-            matrix[i][j] = temp;
-            //System.out.println("");
-            
-            }              
-           /* for(int k = 0 ; k< n; k++){
-            for(int l = 0 ; l< n; l++){
-            System.out.print(" "+matrix[k][l]);}
-            System.out.println("");
-            }*/
-    }
+        int row = n-1, col = n-1;
+        for(int i=0; i<(n+1)/2 ; i++){
+            for(int j= 0;j<n/2;j++){
+                
+              //  System.out.println("i: "+i+" j:"+j +" matrix[row - i][j]"+ matrix[row - i][j] +" matrix[row-i][col-j]"+ matrix[row-i][col-j] +" matrix[i][col-j]"+ matrix[i][col-j] + " matrix[i][j]" + matrix[i][j] );
+                
+                int temp = matrix[col - j][i];
+                
+                matrix[col - j][i] = matrix[row-i][col-j];
+                matrix[row-i][col-j] = matrix[j][row-i];
+                matrix[j][col-i] = matrix[i][j];
+                matrix[i][j] = temp;
+            }
+        }
         
         
     }

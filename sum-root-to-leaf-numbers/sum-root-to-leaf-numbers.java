6c634d440fcd68c1479aa89b903d94d767636d1e
Lakshmi Tanuja Tammireddy <tanujatammireddy@gmail.com>
2021-09-13 21:49:58 -0500

    Time: 0 ms (100.00%), Space: 38.5 MB (17.33%) - LeetHub

--- sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.java
+++ sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.java
@@ -14,27 +14,26 @@
  * }
  */
 class Solution {
-    public int sum =0;
+    public int sum = 0;
     public int sumNumbers(TreeNode root) {
+        if(root == null) 
+            return 0;
         
-        if(root!= null)
-            parse(root,root.val);
+        dfs(root,0);
         
         return sum;
-            
     }
-    public void parse(TreeNode root, int current){
+    public void dfs(TreeNode root, int current_sum){
         
-        if(root.left == null && root.right == null)
-            sum += current;
-        else
-        {
-            if(root.left!= null)
-                parse(root.left,current*10 + root.left.val);
-            
-            if(root.right != null)
-                parse(root.right,current*10 + root.right.val);
-            
-        }
+        current_sum = current_sum*10 + root.val;
+        
+        if(root.left ==  null && root.right == null )
+            sum += current_sum;
+        
+        if(root.left != null)
+            dfs(root.left,current_sum);
+        
+        if(root.right != null)
+            dfs(root.right,current_sum);
     }
 }

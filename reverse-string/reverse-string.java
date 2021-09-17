865f789a1abc486f1acfa3ac9875974bad65f3a7
Lakshmi Tanuja Tammireddy <tanujatammireddy@gmail.com>
2021-09-17 14:50:59 -0500

    Time: 2 ms (23.03%), Space: 52.8 MB (11.21%) - LeetHub

---
+++ reverse-string/reverse-string.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int start =0;
+        int end= s.length-1;
+        while(start < end){
+            char ch = s[start];
+            s[start] = s[end];
+            s[end] = ch;
+            start++;
+            end--;
+        }
+        
+    }
+}

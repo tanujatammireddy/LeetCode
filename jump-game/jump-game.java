d412451155f6136543b10869f23f299cd1064741
Lakshmi Tanuja Tammireddy <tanujatammireddy@gmail.com>
2021-09-18 00:23:29 -0500

    Time: 3 ms (50.02%), Space: 64 MB (16.34%) - LeetHub

--- jump-game/jump-game.java
+++ jump-game/jump-game.java
@@ -1,20 +1,15 @@
 class Solution {
     public boolean canJump(int[] nums) {
         if(nums.length == 1) return true;
-        int[] dp = new int[nums.length];
-        for(int i=0;i<nums.length;i++){
-            int current = nums[i];
-            int j=i+1;
-            while(j< nums.length && current !=0){
-                dp[j]++;
-                j++;
-                current--;
-            }
-            if(i+1< nums.length && dp[i+1] == 0) return false;
+        if(nums[0] == 0) return false;
+        int reachable = 0;
+        
+        for(int i=0;i < nums.length;i++){
+            if(reachable < i) return false;
+            reachable  = Math.max(reachable, i+ nums[i]);
         }
-        if(dp[nums.length-1] == 0)
-        return false;
-        else
-            return true;
+        return true;
+            
     }
+    
 }

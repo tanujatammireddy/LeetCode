4f5d1609142a9600047de5968ff68fa0dea323d8
Lakshmi Tanuja Tammireddy <tanujatammireddy@gmail.com>
2021-09-17 21:03:35 -0500

    Time: 0 ms (100.00%), Space: 35.7 MB (76.11%) - LeetHub

--- climbing-stairs/climbing-stairs.java
+++ climbing-stairs/climbing-stairs.java
@@ -1,18 +1,13 @@
 class Solution {
     public int climbStairs(int n) {
-        
-        int[] memo = new int[n+1];
-        return climbStairWays(0,n,memo);
-    }
-    
-    public int climbStairWays(int i,int n, int[] memo){
-        
-        if(i == n) return 1;
-        if(i > n) return 0;
-        if(memo[i] > 0) return memo[i];
-        
-        memo[i] = climbStairWays(i+1,n,memo) +  climbStairWays(i+2,n,memo);
-        
-        return memo[i];
+        if(n == 1) return 1;
+        if(n == 2) return 2;
+        int[] dp = new int[n+1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i=3;i<= n;i++){
+           dp[i] = dp[i-1]+dp[i-2]; 
+        }
+        return dp[n];
     }
 }

4741fb7d7950de7e28e7f8cabdc17275e79f9d1e
Lakshmi Tanuja Tammireddy <tanujatammireddy@gmail.com>
2021-09-17 23:08:29 -0500

    Time: 0 ms (100.00%), Space: 36.1 MB (89.56%) - LeetHub

---
+++ house-robber-ii/house-robber-ii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int rob(int[] nums) {
+        
+        int n = nums.length;
+        if(n == 1) return nums[0];
+        
+        return Math.max(fill(nums,0,n-2),fill(nums,1,n-1));
+    }
+    public int fill(int[] nums,int start,int n){
+        
+        int house_1 = 0;
+        int house_2 = 0;
+        int max = -1;
+        for(int i= start ;i<=n;i++){
+            max = Math.max( house_1+nums[i],house_2); 
+            house_1 = house_2;
+            house_2 = max;
+        }
+        return max;
+        
+    } 
+}

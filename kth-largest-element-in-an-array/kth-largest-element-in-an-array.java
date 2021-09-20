78f91ab274b481eb15feb9afa548ab8cc66853bf
Lakshmi Tanuja Tammireddy <tanujatammireddy@gmail.com>
2021-09-20 15:38:27 -0500

    Time: 4 ms (67.15%), Space: 39 MB (87.52%) - LeetHub

---
+++ kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> heap = new PriorityQueue<Integer>((a,b)->a-b);
+        for(int num:nums){
+            heap.add(num);
+            if(heap.size() > k)
+                heap.poll();
+        }
+        return heap.poll();
+    }
+}

ca26df4b4ff14524f8c16ff35c5304d0d169f770
Lakshmi Tanuja Tammireddy <tanujatammireddy@gmail.com>
2021-09-21 00:04:08 -0500

    Time: 2 ms (77.50%), Space: 39.8 MB (64.25%) - LeetHub

--- jump-game/jump-game.java
+++ jump-game/jump-game.java
@@ -2,14 +2,14 @@ class Solution {
     public boolean canJump(int[] nums) {
         if(nums.length == 1) return true;
         if(nums[0] == 0) return false;
-        int reachable = 0;
+        int len = nums.length;
+        int max_reach = 0;
         
-        for(int i=0;i < nums.length;i++){
-            if(reachable < i) return false;
-            reachable  = Math.max(reachable, i+ nums[i]);
+        for(int i=0;i<nums.length;i++){
+            if(max_reach < i) return false;
+            max_reach = Math.max(i+nums[i],max_reach);
+            
         }
         return true;
-            
     }
-    
 }

6cfe039b127e361d82db3c44169c00df20b10e4e
Lakshmi Tanuja Tammireddy <tanujatammireddy@gmail.com>
2021-09-20 22:31:31 -0500

    Time: 2 ms (99.60%), Space: 39.1 MB (94.43%) - LeetHub

--- max-area-of-island/max-area-of-island.java
+++ max-area-of-island/max-area-of-island.java
@@ -1,31 +1,29 @@
 class Solution {
-    public int maxArea = 0;
+    int row =0, col=0;
+    int maxArea = 0;
     public int maxAreaOfIsland(int[][] grid) {
-        int area = 0;
-        for(int i=0; i< grid.length;i++){
-            for(int j=0; j< grid[0].length;j++){
-            area = 0;
-            if(grid[i][j]== 1){
-                area = areaOfIsland(grid,i,j,1);
-                maxArea = (area > maxArea)? area :maxArea;
+        row = grid.length;
+        col = grid[0].length;
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(grid[i][j]== 1)
+                    maxArea = Math.max(dfs(grid,i,j,0),maxArea);
             }
         }
-        }
         return maxArea;
-    }
-    public int areaOfIsland(int[][] grid, int x, int y, int area){
-        //System.out.println("area"+area);
-        grid[x][y] = 0;
-        int[][] directions ={{0,1},{1,0},{0,-1},{-1,0}};
         
-        for(int[] dir: directions){
-            
-            int X = x+ dir[0];
-            int Y = y+ dir[1];
-            if(X >= 0 && X < grid.length && Y >= 0 && Y < grid[0].length && grid[X][Y] == 1)
-                area = areaOfIsland(grid,X,Y,area+1);
-            
+    }
+    
+    public int dfs(int[][] grid, int x, int y, int area){
+        if(x>=0 && y>=0 && x<row && y<col && grid[x][y] == 1)
+        {   grid[x][y] = 0;
+            area++;
+            area= dfs(grid,x+1,y,area);
+            area= dfs(grid,x,y+1,area);
+            area= dfs(grid,x,y-1,area);
+            area= dfs(grid,x-1,y,area);
         }
+        
         return area;
     }
 }

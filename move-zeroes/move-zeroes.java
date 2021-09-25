e6fc7902aa0b3b5078518d814f342c2173d1e251
Lakshmi Tanuja Tammireddy <tanujatammireddy@gmail.com>
2021-09-25 00:10:32 -0500

    Time: 1 ms (23.93%), Space: 40.3 MB (36.22%) - LeetHub

--- move-zeroes/move-zeroes.java
+++ move-zeroes/move-zeroes.java
@@ -1,45 +1,21 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        
-        if (nums.length==1) return;
-        
-        int zero_count = 0;
-        for(int num:nums){
-            if(num==0)
-                zero_count++;
-        }
-        
-        int index = 0;
-        int len = nums.length;
-        while(index < len){
-            
-            int start = index;
-            int count = 0;
-            int zeroIndex = index;
-            
-            while(start < nums.length &&  nums[start]== 0){
-                start++;
+    int count = 0;
+    int j = 0;
+        for( int i=0; i<nums.length ;i++){
+            if(nums[i]== 0)
                 count++;
+            else
+            {
+                nums[j] = nums[i];
+                j++;
             }
             
-            int non_ZeroIndex = start--;
-            /*System.out.println("non_ZeroIndex"+non_ZeroIndex);
-            System.out.println("zeroIndex"+zeroIndex);
-            System.out.println("count"+count);*/
-            
-            while(non_ZeroIndex < nums.length){
-                nums[zeroIndex] = nums[non_ZeroIndex];
-                zeroIndex++;
-                non_ZeroIndex++;
-            }
-            len = len-count;
-            index++;
         }
-        
-        while(zero_count !=0){
-            nums[nums.length-zero_count]=0;
-            zero_count --;
-        }
-    
+    for(int k= nums.length-1; count > 0 ;k--){
+        nums[k]=0;
+        count--;
     }
+    }
+        
 }

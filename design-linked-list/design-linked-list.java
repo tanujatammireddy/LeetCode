4c7e9fb22480b6554263962f3cba9647b8838b02
Lakshmi Tanuja Tammireddy <tanujatammireddy@gmail.com>
2021-10-21 23:52:34 -0500

    Time: 14 ms (28.88%), Space: 47.9 MB (21.78%) - LeetHub

---
+++ design-linked-list/design-linked-list.java
@@ -0,0 +1,64 @@
+public class ListNode{
+    int val;
+    ListNode next;
+    ListNode(int x){
+        val = x;
+    }
+}
+class MyLinkedList {
+    int size ;
+    ListNode head;
+    public MyLinkedList() {
+        head = new ListNode(-1);
+        size = 0;
+    }
+    
+    public int get(int index) {
+        if(index < 0 || index >= size) return -1;
+        ListNode current = head;
+        for(int i = 0; i< index+1 ;i++)
+            current = current.next;
+        return current.val;
+    }
+    
+    public void addAtHead(int val) {
+        addAtIndex(0,val);
+    }
+    
+    public void addAtTail(int val) {
+        addAtIndex(size,val);
+    }
+    
+    public void addAtIndex(int index, int val) {
+        if(index < 0 || index > size) return;
+        ListNode current = head;
+        
+        for(int i = 0; i< index ;i++)
+            current = current.next;
+        ListNode newNode = new ListNode(val);
+        
+        newNode.next = current.next;
+        current.next = newNode;
+        size++;
+    }
+    
+    public void deleteAtIndex(int index) {
+        if(index < 0 || index >= size) return;
+        ListNode current = head;
+        
+        for(int i = 0; i< index ;i++)
+            current = current.next;
+        size--;
+        current.next = current.next.next;
+    }
+}
+
+/**
+ * Your MyLinkedList object will be instantiated and called as such:
+ * MyLinkedList obj = new MyLinkedList();
+ * int param_1 = obj.get(index);
+ * obj.addAtHead(val);
+ * obj.addAtTail(val);
+ * obj.addAtIndex(index,val);
+ * obj.deleteAtIndex(index);
+ */

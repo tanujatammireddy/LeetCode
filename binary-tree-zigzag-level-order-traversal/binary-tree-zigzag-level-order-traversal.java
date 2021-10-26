d0b3502f770ce164fe1039833692fb422abc5624
Lakshmi Tanuja Tammireddy <tanujatammireddy@gmail.com>
2021-10-25 19:26:07 -0500

    Time: 3 ms (8.14%), Space: 40 MB (7.46%) - LeetHub

---
+++ binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> returnList = new ArrayList();
+        returnList = bfs(root);
+        for(int i=0;i< returnList.size();i++ ){
+            if(i%2 == 1){
+                List<Integer> list = returnList.get(i);
+                Collections.reverse(list);
+            }
+        }
+        return returnList;
+    }
+    public List<List<Integer>> bfs(TreeNode root){
+        List<List<Integer>> subList = new ArrayList();
+        if(root != null){
+            
+            Queue<Pair<TreeNode,Integer>> queue = new LinkedList();
+            queue.add(new Pair<>(root,0));
+            
+            while(!queue.isEmpty()){
+                
+                Pair p = queue.poll();
+                TreeNode currentNode = (TreeNode)p.getKey();
+                int currentLevel = (Integer)p.getValue();
+                
+                if(subList.isEmpty() ||currentLevel >= subList.size()){
+                    List<Integer> list = new ArrayList();
+                    subList.add(list);
+                } 
+                subList.get(currentLevel).add(currentNode.val);
+                        
+                if(currentNode.left != null)
+                    queue.add(new Pair(currentNode.left,currentLevel+1));
+                if(currentNode.right != null)
+                    queue.add(new Pair(currentNode.right,currentLevel+1));
+                    
+            }
+        }
+        return subList;
+    }
+}

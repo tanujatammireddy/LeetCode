9ec27fdec2d430ef3d7dd60ba9eb1684961049e2
Lakshmi Tanuja Tammireddy <tanujatammireddy@gmail.com>
2021-10-26 00:34:21 -0500

    Time: 471 ms (16.40%), Space: 126 MB (5.00%) - LeetHub

--- longest-palindromic-substring/longest-palindromic-substring.java
+++ longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,45 +1,36 @@
 class Solution {
     public String longestPalindrome(String s) {
         
-        String returnString ="";
-        if(s == null || s.length() < 1) return "";
-        if(s.length()==1) return s;
         int len = s.length();
-        boolean [][] memo = new boolean[len][len];
+        int[][] table = new int[len][len];
+        int maxLength = 1;
         
-        for(int start =0; start <len; start++){
-            int j = start;
-            memo[start][j] = true;
-            if(j+1 < len && s.charAt(start) == s.charAt(j+1)){
-                j++;
-               memo[start][j] = true;
-            }
-            if(memo[start][j] == true && j-start >= returnString.length())
-                returnString = s.substring(start,j+1);
-             
+        for(int i=0;i< len;i++){
+            table[i][i] = 1;
         }
-        for(int k=3; k<=len; k++){
-        for(int start =0; start <len; start++){
-        int end = start + k -1;
-            if(end < len){
-               if(s.charAt(start)== s.charAt(end)){
-                    if(start+1 < len && end -1 >=0 && memo[start+1][end-1] == true){
-                            memo[start][end] = true;
-                    }
-                }
-                if(memo[start][end] == true && end-start >= returnString.length()){
-                            returnString = s.substring(start,end+1);
-                } 
-            }   
-            }
-            }
-      /* for(int i=0;i<len;i++){
-            for(int j=0;j<len;j++){
-                    System.out.print(" "+memo[i][j]);
-                }
-            System.out.println("");
-         }*/
-        return returnString;
         
+        int start = 0;
+        for(int i=0;i<len-1;i++){
+            if(s.charAt(i)== s.charAt(i+1)){
+                table[i][i+1] = 1;
+                maxLength = 2;
+                start = i;
+            }
+        }
+        
+        for(int k= 3; k<=len;k++){
+            for(int i=0;i<len-k+1;i++){
+                int j = i+k-1;
+                if(table[i+1][j-1] == 1 && s.charAt(i)== s.charAt(j)){
+                    table[i][j] = 1;
+                if(k > maxLength){
+                    maxLength = k;
+                    start = i;
+                }
+                }
+            }
+        }
+        
+        return s.substring(start,start+maxLength);
     }
 }

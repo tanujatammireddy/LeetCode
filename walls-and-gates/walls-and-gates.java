71f1851f19d43b6e8dfa19db1fb0a52b7fe07c93
Lakshmi Tanuja Tammireddy <tanujatammireddy@gmail.com>
2021-10-31 17:33:49 -0500

    Time: 153 ms (9.82%), Space: 184.7 MB (5.81%) - LeetHub

--- walls-and-gates/walls-and-gates.java
+++ walls-and-gates/walls-and-gates.java
@@ -1,32 +1,33 @@
 class Solution {
     public void wallsAndGates(int[][] rooms) {
-    if(rooms == null || rooms.length == 0 || rooms[0].length == 0)
-        return;
-           
-    for(int i = 0; i < rooms.length; i++) {
-        for(int j = 0; j < rooms[0].length; j++) {
-            if(rooms[i][j] == 0){
-                int dis = 0;
-                helper(rooms, i, j, dis);
+        
+        Queue<int[]> queue = new LinkedList<int[]>();
+        
+        for(int i=0; i<rooms.length; i++){
+            for(int j=0; j<rooms[0].length; j++){
+                if(rooms[i][j] == 0)
+                    queue.add(new int[]{i,j,0});
+            } 
+        }
+        
+        if(!queue.isEmpty()){
+        int[][] directions = new int[][]{{0,1},{0,-1},{1,0},{-1,0}};
+        while(!queue.isEmpty()){
+            int[] current = queue.poll();
+            int x = current[0];
+            int y = current[1];
+            //System.out.print(Arrays.toString(current));
+            int distance = current[2];
+            rooms[x][y] = Math.min(rooms[x][y],distance); 
+            for(int[] dir:directions){
+                int X = x + dir[0];
+                int Y = y + dir[1];
+                if(X >= 0 && Y>=0 && X< rooms.length && Y< rooms[0].length && rooms[X][Y] == 2147483647)
+                    queue.add(new int[]{X,Y,distance+1});
             }
+        }
         }
     }
     
-    return;
-}
-public void helper(int[][] rooms, int i, int j, int dis) {
-    int[] X = {0, 1, 0, -1};
-    int[] Y = {1, 0, -1, 0};
     
-    
-    for(int k = 0; k < 4; k++) {
-        if(inBounds(rooms, i + X[k], j + Y[k]) && dis + 1 < rooms[i + X[k]][j + Y[k]]) {
-            rooms[i + X[k]][j + Y[k]] = dis + 1;
-            helper(rooms, i + X[k], j + Y[k], dis + 1);
-        }                               
-    }
-}
-public boolean inBounds(int[][] rooms, int x, int y) {
-    return x >= 0 && x < rooms.length && y >= 0 && y < rooms[0].length;
-}
 }

00f4b5a3dd007cc93bde0e034a29e698feb2b523
Lakshmi Tanuja Tammireddy <tanujatammireddy@gmail.com>
2021-11-04 16:16:46 -0500

    Time: 4 ms (21.99%), Space: 39.2 MB (67.21%) - LeetHub

--- populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.java
+++ populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.java
@@ -23,42 +23,32 @@ class Node {
 
 class Solution {
     public Node connect(Node root) {
-    if(root != null){
         
-        Node start = root;
-        Queue<Pair<Node,Integer>> queue = new LinkedList();
-        queue.add(new Pair(start,0));
-        List<List<Node>> nodeList = new ArrayList();
+        Queue<Pair<Node,Integer>> queue = new LinkedList<>();
+        if(root != null)
+        queue.add(new Pair(root,0));
+        int previousLevel = -1;
+        Node previousNode = new Node();
+        
         while(!queue.isEmpty()){
+            
             Pair p = queue.poll();
-            Node current = (Node) p.getKey();
-            int currentLevel = (int) p.getValue();
-            if(nodeList.size() == currentLevel){
-                List<Node> subList = new ArrayList();
-                subList.add(current);
-                nodeList.add(subList);
-            }
-            else
-            nodeList.get(currentLevel).add(current);
+            Node currentNode = (Node)p.getKey();
+            int currentLevel = (Integer) p.getValue();
             
-            if(current.left != null)
-            queue.add(new Pair(current.left,currentLevel+1));
-            if(current.right != null)
-            queue.add(new Pair(current.right,currentLevel+1));
+            if(previousLevel == currentLevel)
+                currentNode.next = previousNode;
+            if(currentNode.right != null)
+                queue.add(new Pair(currentNode.right,currentLevel+1));
+            if(currentNode.left != null)
+                queue.add(new Pair(currentNode.left,currentLevel+1));
             
-        }
-        for(int i = 1; i< nodeList.size(); i++){
-            List<Node> currentList = nodeList.get(i);
-            int len = currentList.size();
-            Node previous = currentList.get(len-1);
-            for(int j = len-2; j>=0 ;j--){
-                Node current = currentList.get(j);
-                current.next = previous;
-                previous = current;
-            }
+            previousNode = currentNode;
+            previousLevel = currentLevel;
+            
         }
         
-    }
-    return root;
+        
+        return root;
     }
 }

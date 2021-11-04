4ef017d5db45878593865c78d19b4ce683b7ecac
Lakshmi Tanuja Tammireddy <tanujatammireddy@gmail.com>
2021-11-04 13:27:39 -0500

    Time: 0 ms (100.00%), Space: 38.7 MB (69.18%) - LeetHub

--- search-insert-position/search-insert-position.java
+++ search-insert-position/search-insert-position.java
@@ -1,18 +1,17 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
+        
         int low = 0, high = nums.length-1;
-        int mid;
+        int mid ;
         while(low <= high){
-            mid = low + (high-low)/2;
+            mid = (low +high)/2;
             if(nums[mid] == target)
                 return mid;
             else if(nums[mid] < target)
                 low = mid+1;
             else
                 high = mid-1;
-            
         }
-        System.out.println("low"+low+" high: "+high);
         return low;
     }
 }

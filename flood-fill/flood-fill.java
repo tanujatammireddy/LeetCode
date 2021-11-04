df7c5cd7435b24e8292cf100974d055f1b8521ac
Lakshmi Tanuja Tammireddy <tanujatammireddy@gmail.com>
2021-11-04 15:38:10 -0500

    Time: 1 ms (62.78%), Space: 39.7 MB (90.85%) - LeetHub

--- flood-fill/flood-fill.java
+++ flood-fill/flood-fill.java
@@ -1,24 +1,29 @@
 class Solution {
-    public int row, col;
+    int row ;
+    int col;
+    int[][] directions;
     public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
-        int[][] direction = {{-1,0},{0,-1},{1,0},{0,1}};
-        int oldColor = image[sr][sc];
-        if(oldColor != newColor){
+        
         row = image.length;
         col = image[0].length;
-        dfs(image,direction,sr,sc,newColor,oldColor);
-        }
+        int oldColor   = image[sr][sc];
+        directions = new int[][]{{0,1},{0,-1},{1,0},{-1,0}};
+        if(newColor != oldColor)
+        backtracking(image,sr,sc,newColor,oldColor);
         return image;
     }
-    public void dfs(int[][] image,int[][] direction, int sr, int sc, int newColor, int oldColor){
+    public void backtracking(int[][] image,int x, int y,int newColor,int oldColor){
         
-        image[sr][sc] = newColor;
-        for(int[] dir: direction){
-            int x = sr + dir[0];
-            int y = sc + dir[1];
+        if(x >= 0 && x < row && y >= 0 && y< col && image[x][y] == oldColor){
             
-            if(x >= 0 && x< row && y>=0 && y<col && image[x][y] == oldColor)
-                dfs(image,direction,x,y,newColor,oldColor);
+            image[x][y] = newColor;
+            
+            for(int[] direction:directions ){
+                int X = x + direction[0];
+                int Y = y + direction[1];
+                
+                backtracking(image,X,Y,newColor,oldColor);
+            }
         }
     }
 }

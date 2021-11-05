8394c820cefd01854b6e5709e759f761529cec88
Lakshmi Tanuja Tammireddy <tanujatammireddy@gmail.com>
2021-11-04 22:24:48 -0500

    Time: 5 ms (46.44%), Space: 36.1 MB (81.65%) - LeetHub

---
+++ arranging-coins/arranging-coins.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int arrangeCoins(int n) {
+        
+        int row = 1;
+        while( n >= row){
+            n = n- row;
+            row++;
+        }
+        return row-1;
+    }
+}
+

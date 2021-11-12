8068bdc8225ea8124e0a73b86edfd10b7efbd921
Lakshmi Tanuja Tammireddy <tanujatammireddy@gmail.com>
2021-11-12 12:09:44 -0600

    Time: 263 ms (26.21%), Space: 43.4 MB (50.61%) - LeetHub

--- 3sum/3sum.java
+++ 3sum/3sum.java
@@ -1,28 +1,33 @@
 class Solution {
-    
     public List<List<Integer>> threeSum(int[] nums) {
         
-        Arrays.sort(nums);
         List<List<Integer>> returnList = new ArrayList();
+        Arrays.sort(nums);
         
-        for(int i=0;i<nums.length ;i++){
-            if(i==0 || nums[i-1] != nums[i])
-           twoSum(nums,i,returnList);
-        }
+        for(int i= 0;i< nums.length ;i++)
+            if(i== 0 || nums[i-1]!= nums[i])
+                twoSum(nums,i,-nums[i],returnList);
+
         return returnList;
     }
-    public void twoSum(int[] nums, int pos,List<List<Integer>> returnList) {
+    
+    public void twoSum(int[] nums, int current , int sum, List<List<Integer>>  returnList){
         
         Set<Integer> set = new HashSet();
         
-        for(int i=pos+1; i<nums.length ;i++){
-            int complemenet = -nums[pos]-nums[i];
-            if(set.contains(complemenet)){
-                returnList.add(Arrays.asList(nums[i],complemenet,nums[pos]));
-                while(i+1 < nums.length && nums[i]== nums[i+1])
-                    i++;
+        for(int i= current+1;i< nums.length ;i++){
+            
+            if(set.contains(sum-nums[i])){
+                returnList.add(Arrays.asList(nums[current],sum-nums[i],nums[i]));
+                while(i+1< nums.length && nums[i] == nums[i+1])
+                i++;
             }
-             set.add(nums[i]);
-            }
+            else
+                set.add(nums[i]);
+            
+            
+          
+        }
+        
     }
 }

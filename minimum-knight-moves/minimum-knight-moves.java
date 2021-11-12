34b24b13c81b683898f5c3f40f9e1fd37e57c42d
Lakshmi Tanuja Tammireddy <tanujatammireddy@gmail.com>
2021-11-11 23:48:55 -0600

    Time: 126 ms (75.74%), Space: 46.3 MB (65.05%) - LeetHub

--- minimum-knight-moves/minimum-knight-moves.java
+++ minimum-knight-moves/minimum-knight-moves.java
@@ -1,35 +1,42 @@
 class Solution {
+    int[][] directions;
+    boolean[][] visited = new boolean[605][605];
     public int minKnightMoves(int x, int y) {
-        int[][] directions ={ {-1,-2},{1,-2},{-1,2},{1,2},{-2,-1},{2,1},{2,-1},{-2,1}};
-        return bfs(directions,x,y);
+        directions =new int[][] { {-1,-2},{1,-2},{-1,2},{1,2},{-2,-1},{2,1},{2,-1},{-2,1}};
+        return bfs(x,y);
     }
    
-    public int bfs(int[][] directions,int final_x,int final_y ){
+    public int bfs(int x, int y){
+        
+        Queue<int[]> queue = new LinkedList<int[]>();
+        queue.add(new int[]{0,0});
         
         int steps = 0;
-        Deque<int[]> queue = new ArrayDeque<int[]>();
-        queue.add(new int[]{0, 0});
-        
-        boolean[][] visited = new boolean[605][605];
         
         while(!queue.isEmpty()){
-            int currQueueSize = queue.size();
-            for(int i=0; i<currQueueSize; i++){
-            int[] curr = queue.poll();
-            if(curr[0] == final_x && curr[1] == final_y)
+            
+            int size = queue.size();
+            
+            for(int i=0; i<size; i++){
+                int[] current = queue.poll();
+                int X = current[0];
+                int Y = current[1];
+    
+                if(X == x && Y == y)
                     return steps;
-                
-            for(int[] dir:directions){
-                int[] next = new int[]{curr[0]+dir[0], curr[1]+dir[1]};
-                if (!visited[next[0] + 302][next[1] + 302]) {
-                    visited[next[0] + 302][next[1] + 302] = true;
-                    queue.addLast(next);
-                }    
+            
+                for(int[] dir:directions ){
+                    
+                    if(!visited[302+X+dir[0]][302+Y+dir[1]]){
+                        
+                        visited[302+X+dir[0]][302+Y+dir[1]]= true;
+                        queue.add(new int[]{X+dir[0],Y+dir[1]});
+                        
+                    }
+                }
             }
+           steps++; 
         }
-            steps++;
-        }
-        
         return steps;
     }
 }

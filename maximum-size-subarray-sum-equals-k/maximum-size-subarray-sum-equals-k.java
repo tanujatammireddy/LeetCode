c10dcedc5a94d9b14c841efe971fc55e5934926c
Lakshmi Tanuja Tammireddy <tanujatammireddy@gmail.com>
2021-11-17 11:31:04 -0600

    Time: 30 ms (94.90%), Space: 68.8 MB (48.19%) - LeetHub

--- maximum-size-subarray-sum-equals-k/maximum-size-subarray-sum-equals-k.java
+++ maximum-size-subarray-sum-equals-k/maximum-size-subarray-sum-equals-k.java
@@ -9,14 +9,16 @@ class Solution {
         for(int i=0;i<nums.length ;i++){
             
             prefix += nums[i]; 
-            
+
             if(prefix == k)
                maxSubArrayLen = i+1; 
             
             if(map.containsKey(prefix-k))
                 maxSubArrayLen = Math.max(maxSubArrayLen, i- map.get(prefix-k));
+            
             if(nums[i] == k)
                 maxSubArrayLen = Math.max(maxSubArrayLen, 1);
+            
             if(!map.containsKey(prefix))
                 map.put(prefix,i);
         }

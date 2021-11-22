f5cd244188f8d2cd7203886462b9b68da3e41209
Lakshmi Tanuja Tammireddy <tanujatammireddy@gmail.com>
2021-11-22 16:49:48 -0600

    Time: 1 ms (66.08%), Space: 41 MB (20.28%) - LeetHub

--- best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -2,30 +2,13 @@ class Solution {
     public int maxProfit(int[] prices) {
         
         int maxProfit = 0;
-        List<Integer> peekValley = new ArrayList();
-        
         for(int i=1; i< prices.length ;i++){
-            
-            int current = prices[i]- prices[i-1];
-            if(peekValley.isEmpty())
-                peekValley.add(current);
-            else{
-                int lastValue = peekValley.get(peekValley.size() -1);
-                if(lastValue < 0 && current < 0)
-                    peekValley.set(peekValley.size() -1, (lastValue+current));
-                else if(lastValue > 0 && current > 0)
-                    peekValley.set(peekValley.size() -1, (lastValue+current));
-                else
-                    peekValley.add(current);
-            }
+            if(prices[i]- prices[i-1] > 0)
+                maxProfit += prices[i]- prices[i-1];
         }
         
-        //System.out.print(peekValley);
-        for(int i=0;i<peekValley.size() ; i++){
-            if(peekValley.get(i) >0)
-                maxProfit += peekValley.get(i);
-        }
         return maxProfit; 
         
     }
 }
+  

b4f49fe53c1d6ce8c781229008eba66f6103c501
Lakshmi Tanuja Tammireddy <tanujatammireddy@gmail.com>
2021-11-21 22:28:26 -0600

    Time: 2 ms (74.90%), Space: 37.4 MB (99.24%) - LeetHub

--- add-binary/add-binary.java
+++ add-binary/add-binary.java
@@ -1,23 +1,68 @@
 class Solution {
-  public String addBinary(String a, String b) {
-    int n = a.length(), m = b.length();
-    if (n < m) return addBinary(b, a);
-    int L = Math.max(n, m);
-
-    StringBuilder sb = new StringBuilder();
-    int carry = 0, j = m - 1;
-    for(int i = L - 1; i > -1; --i) {
-      if (a.charAt(i) == '1') ++carry;
-      if (j > -1 && b.charAt(j--) == '1') ++carry;
-
-      if (carry % 2 == 1) sb.append('1');
-      else sb.append('0');
-
-      carry /= 2;
+    public String addBinary(String a, String b) {
+        
+        StringBuilder sb = new StringBuilder();
+        
+        int i= a.length()-1, j= b.length()-1;
+        char carry = '0';
+        char char_a = '0';
+        char char_b = '0';
+        
+        while(i >= 0  || j >=0 ){
+            
+            char_a = i >= 0 ? a.charAt(i) : '0';
+            char_b = j >= 0 ? b.charAt(j) : '0';
+            
+            char sum = '0';
+            
+            //System.out.println("char_a: "+char_a+" char_b: "+char_b+" carry: "+carry);
+            if( (char_a == '0' && char_b == '1') ||(char_a == '1' && char_b == '0')){
+                if(carry == '0')
+                {
+                    sum = '1';
+                    carry = '0';
+                    
+                }
+                else
+                {
+                    sum = '0';
+                    carry = '1';
+                }
+                }
+            else if(char_a == '0' && char_b == '0'){
+                 if(carry == '0')
+                {   
+                    carry = '0';
+                    sum = '0';
+                }
+                else
+                {
+                    sum = '1';
+                    carry = '0';
+                }
+                   
+               }
+               // char_a = 1, char_b = 1
+           else {
+                if(carry == '0')
+                {
+                    sum = '0';
+                    carry = '1';
+                }
+                else
+                {
+                    sum = '1';
+                    carry = '1';
+                }
+               }
+               
+            //System.out.println("sum"+sum);
+            sb.insert(0,sum);
+            i--; j--;
+               }
+        if(carry == '1')
+            sb.insert(0,carry);
+        return sb.toString();
+        
     }
-    if (carry == 1) sb.append('1');
-    sb.reverse();
-
-    return sb.toString();
-  }
 }

9d32e7f8b2764cef0a1b4f32dd6e6f793e3f9755
Lakshmi Tanuja Tammireddy <tanujatammireddy@gmail.com>
2021-11-22 15:15:38 -0600

    Time: 22 ms (18.80%), Space: 42.4 MB (12.60%) - LeetHub

--- basic-calculator-ii/basic-calculator-ii.java
+++ basic-calculator-ii/basic-calculator-ii.java
@@ -1,56 +1,72 @@
 class Solution {
     public int calculate(String s) {
-        s.trim();
-        int len = s.length() ;
-        Stack<Integer> stack = new Stack<Integer>();
-        char operator = '$';
         
-        for(int i = 0 ;i< len; i++){
+        s = s.replace(" ","");
+        Stack<Integer> stack = new Stack();
+        int sign = 1;
+        int i= 0;
+        while(i<s.length()){
             
-            if(s.charAt(i) == ' ') continue;
+            char ch = s.charAt(i);
             
-            StringBuilder sb = new StringBuilder();
-        
-            if(Character.isDigit(s.charAt(i))){
-            
-            while(i < len && Character.isDigit(s.charAt(i))){
-                sb.append(s.charAt(i));
-                i++;   
-            }
-            
-            int value = Integer.parseInt(sb.toString());
-             
-            if(!stack.isEmpty() && (operator == '*'||operator == '/')){
-                value = operation(stack.pop(),value,operator); 
+            if(ch == '*' || ch == '/'){
                 
-            }
+                int firstEle = stack.pop();
                 
-            else if(operator == '-' )
-                value = value*-1;
-           
-            stack.push(value);
-               i--; 
+                int j= i+1;
+                int secondEle = 0;
+                 while(j < s.length() && Character.isDigit(s.charAt(j)) )
+                {
+                    secondEle = secondEle*10 + Character.getNumericValue(s.charAt(j));
+                    j++; 
+                }
+                //System.out.println("secondEle"+secondEle);
+                
+                int result = 0;
+                if(ch == '*')
+                    result = firstEle*secondEle;
+                else
+                    result = firstEle/secondEle;
+                
+                i= j;
+                stack.push(result);
             }
-            else
-                operator = s.charAt(i);
-            
-            
+            else if(ch == '-' || ch == '+'){
+                int j = i+1;
+                int num = 0;
+                while(j < s.length() && Character.isDigit(s.charAt(j)) )
+                {
+                    num = num*10 + Character.getNumericValue(s.charAt(j));
+                    j++;
+                }
+                if(ch == '-')
+                    stack.push(-num);
+                else
+                    stack.push(num);
+                
+                i= j;
+                //System.out.println("*******num"+num);
             }
-            
+            else{
+                
+                int j= i;
+                int num = 0;
+                 while(j < s.length() && Character.isDigit(s.charAt(j)) )
+                {
+                    num = num*10 + Character.getNumericValue(s.charAt(j));
+                    j++; 
+                }
+                //System.out.println("num"+num);
+                stack.push(num);
+                i= j;
+            }
+        }
         int result = 0;
-        while(! stack.isEmpty()){
-            
+        while(!stack.isEmpty()){
+            //System.out.println(stack.peek());
             result += stack.pop();
         }
-        return result;
-    }
-    public int operation (int a, int b , char ch){
-        if(ch == '/')
-            return a/b;
-        else if(ch == '*')
-            return a*b;
-        else
-            return 0;
         
+        return result;
     }
 }

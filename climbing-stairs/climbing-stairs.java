3c6a126d4d9f4bd85dd8fdfee495fae82919200e
Lakshmi Tanuja Tammireddy <tanujatammireddy@gmail.com>
2021-11-22 22:51:29 -0600

    Time: 0 ms (100.00%), Space: 35.6 MB (80.06%) - LeetHub

--- climbing-stairs/climbing-stairs.java
+++ climbing-stairs/climbing-stairs.java
@@ -1,25 +1,27 @@
 class Solution {
     
+   
     public int climbStairs(int n) {
         
-        Integer[] dp = new Integer[n+1];
-        return fill_array(dp,n);
+         Integer[] dp = new Integer[n+1];
+        return count(dp,n);
         
     }
-    public int fill_array(Integer[] dp,int n)
-    {   
+    public int count(Integer[] dp, int n){
+        
         if(dp[n] != null)
             return dp[n];
-        else if(n<=0)
-            dp[n] = 0;
+        if(n <= 0)
+            return 0;
         else if(n == 1)
             dp[n] = 1;
         else if(n == 2)
             dp[n] = 2;
         else
-            dp[n] = fill_array(dp,n-1)+ fill_array(dp,n-2);
-     
+            dp[n] = count(dp,n-1) + count(dp,n-2);
+        
         return dp[n];
+        
+        
     }
-    
 }

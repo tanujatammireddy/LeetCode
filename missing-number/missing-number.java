66b42d6e7fffecf6436d32e8c669ba0ec6d80817
Lakshmi Tanuja Tammireddy <tanujatammireddy@gmail.com>
2021-11-23 18:13:06 -0600

    Time: 1 ms (57.00%), Space: 48.3 MB (15.76%) - LeetHub

---
+++ missing-number/missing-number.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        
+        int n = nums.length ;
+        int sum = (n*(n+1))/2;
+        for(int num:nums)
+            sum -= num;
+        return sum;
+    }
+}

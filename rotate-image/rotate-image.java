78e284ab3a9400666864b04e66c360b55f1f47d9
Lakshmi Tanuja Tammireddy <tanujatammireddy@gmail.com>
2021-11-29 20:30:04 -0600

    Time: 0 ms (100.00%), Space: 38.9 MB (87.98%) - LeetHub

--- rotate-image/rotate-image.java
+++ rotate-image/rotate-image.java
@@ -1,22 +1,26 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        
+        transpose(matrix);
+        reflect(matrix);
+    }
+    public void transpose(int[][] matrix) {
         int n = matrix.length;
-        int row = n-1, col = n-1;
-        for(int i=0; i<(n+1)/2 ; i++){
-            for(int j= 0;j<n/2;j++){
-                
-              //  System.out.println("i: "+i+" j:"+j +" matrix[row - i][j]"+ matrix[row - i][j] +" matrix[row-i][col-j]"+ matrix[row-i][col-j] +" matrix[i][col-j]"+ matrix[i][col-j] + " matrix[i][j]" + matrix[i][j] );
-                
-                int temp = matrix[col - j][i];
-                
-                matrix[col - j][i] = matrix[row-i][col-j];
-                matrix[row-i][col-j] = matrix[j][row-i];
-                matrix[j][col-i] = matrix[i][j];
+        for (int i = 0; i <  n; i ++) {
+            for (int j = i+1; j < n; j++) {
+                int temp = matrix[j][i];
+                matrix[j][i] = matrix[i][j];
                 matrix[i][j] = temp;
             }
         }
-        
-        
+    }
+    public void reflect(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n; i ++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = matrix[i][n-j-1];
+                matrix[i][n-j-1] = matrix[i][j];
+                matrix[i][j] = temp;
+            }
+        }
     }
 }

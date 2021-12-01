012187780c4f8a01188e70cba2baaba10ca27fdc
Lakshmi Tanuja Tammireddy <tanujatammireddy@gmail.com>
2021-11-30 20:19:40 -0600

    Time: 1 ms (93.58%), Space: 37.4 MB (69.07%) - LeetHub

--- decode-ways/decode-ways.java
+++ decode-ways/decode-ways.java
@@ -1,24 +1,27 @@
 class Solution {
     public int numDecodings(String s) {
         
-        if(s.charAt(0) == '0') return 0;
+        if(s.charAt(0) == '0')
+            return 0;
         
         int len = s.length();
         int[] dp = new int[len+1];
         dp[0] = 1;
         dp[1] = 1;
         
-        for(int i= 2;i < len+1 ;i++ ){
+        for(int i= 2; i<= len ;i++){
+            
+            //System.out.print(i);
             if(s.charAt(i-1) != '0')
                 dp[i] = dp[i-1];
             
-            int two_digit = Integer.parseInt(s.substring(i-2,i));
-            if(two_digit >= 10 && two_digit<= 26)
-                dp[i] += dp[i-2]; 
+            //System.out.print(dp[i]);
+            
+            int twoDigit = Integer.parseInt(s.substring(i-2,i));
+            if(twoDigit >= 10 && twoDigit <= 26) 
+                dp[i] += dp[i-2];
         }
         
         return dp[len];
-            
-        
     }
 }

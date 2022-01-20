49b1b91e1dab2d40640502ad7710ced363319cba
Lakshmi Tanuja Tammireddy <tanujatammireddy@gmail.com>
2022-01-20 10:14:01 -0600

    Time: 15 ms (5.41%), Space: 39.4 MB (5.25%) - LeetHub

--- 388-longest-absolute-file-path/388-longest-absolute-file-path.java
+++ 388-longest-absolute-file-path/388-longest-absolute-file-path.java
@@ -1,35 +1,23 @@
 class Solution {
     public int lengthLongestPath(String input) {
-        int maxLength=0;
+        
         String[] paths=input.split("\n");
-        Stack<PathVal> st=new Stack<>();
-        for(String path:paths){
-            int count=0;
-            for(int i=0;i<path.length();i++){
-                char c=path.charAt(i);
-                if(c=='\t'){
-                    count++;
-                }
+        
+        int[] stack = new int[paths.length + 1];
+        stack[0] = -1;
+
+        int maxLen = 0;
+        for (String s : paths) {
+            int lev = s.lastIndexOf("\t") + 2;
+            
+            stack[lev] = stack[lev-1] + s.length() - lev + 2;
+            
+            System.out.println(s+" lev: "+lev+" stack[lev]:"+stack[lev]);
+            if (s.contains(".")) {
+                maxLen = Math.max(maxLen, stack[lev]);
             }
-             while(!st.empty()&&(st.peek().count>=count)){
-                st.pop();
-            }
-            int length=path.length()+(st.empty()?0:st.peek().length);
-            length=length-count;
-            if(!st.empty())length++;
-            PathVal pathVal=new PathVal(count,length);
-            if(path.contains(".")){
-                maxLength=Math.max(maxLength,pathVal.length);
-            }
-            st.push(pathVal);
         }
-        return maxLength;
-    }
-}
-class PathVal{
-    int count,length;
-    public PathVal(int count,int length){
-        this.count=count;
-        this.length=length;
+
+        return maxLen;
     }
 }

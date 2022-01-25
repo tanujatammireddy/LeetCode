e4b39727f5399f5a9369b709d435652a733fdc4f
Lakshmi Tanuja Tammireddy <tanujatammireddy@gmail.com>
2022-01-25 14:25:26 -0600

    Time: 3 ms (27.90%), Space: 51.8 MB (13.58%) - LeetHub

---
+++ 941-valid-mountain-array/941-valid-mountain-array.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean validMountainArray(int[] arr) {
+        
+        boolean peak = false;
+        boolean valley = false;
+        if(arr.length == 1)
+            return false;
+        for(int i= 1;i< arr.length; i++){
+            
+            if(arr[i-1]== arr[i])
+                return false;
+            if(arr[i-1] < arr[i]){
+                if(valley)
+                    return false;
+                peak = true;
+            }
+            if(arr[i-1] > arr[i]){
+                valley = true;
+                if(!peak)
+                    return false;
+            }
+            
+            
+            
+        }
+        
+        return valley && peak;
+    }
+}

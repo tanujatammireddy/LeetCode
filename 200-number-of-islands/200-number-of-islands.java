7b076bbe62fdb2eff0fc18b06207e40ecd72f0c2
Lakshmi Tanuja Tammireddy <tanujatammireddy@gmail.com>
2022-02-05 19:09:21 -0600

    Time: 2 ms (99.96%), Space: 50.9 MB (60.86%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,17 +1,11 @@
 class Solution {
     
-    int row;
-    int col;
-    
+   
     public int numIslands(char[][] grid) {
         
         int count = 0;
-        row = grid.length;
-        col = grid[0].length;
-        
-        for(int i= 0; i<row; i++){
-          for(int j= 0; j<col; j++){
-              
+        for(int i= 0; i<grid.length; i++){
+          for(int j= 0; j<grid[0].length; j++){
               if(grid[i][j] == '1')
               {
                   helper(grid,i,j);
@@ -19,12 +13,11 @@ class Solution {
               }
         }  
         }
-        
         return count;
     }
     
     public void helper(char[][] grid, int x, int y){
-        if(x>=0 && x< row && y>=0 && y< col && grid[x][y] =='1')
+        if(x>=0 && x< grid.length && y>=0 && y< grid[0].length && grid[x][y] =='1')
         {
             grid[x][y] ='0';
             helper(grid,x+1,y);

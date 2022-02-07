dfa3a9d3b7a2d24b2bba98ff317ea428f5e76345
Lakshmi Tanuja Tammireddy <tanujatammireddy@gmail.com>
2022-02-06 19:21:19 -0600

    Time: 2 ms (32.88%), Space: 41.7 MB (9.49%) - LeetHub

---
+++ 433-minimum-genetic-mutation/433-minimum-genetic-mutation.java
@@ -0,0 +1,61 @@
+class Solution {
+    public int minMutation(String start, String end, String[] bank) {
+        
+        Set<String> geneBank = new HashSet();
+        for(int i=0;i< bank.length;i++){
+            geneBank.add(bank[i]);
+        }
+       // if(!geneBank.contains(end)) return -1;
+        
+        int count = 0;
+        Queue<String> queue = new LinkedList();
+        queue.add(start);
+        while(!queue.isEmpty()){
+            
+            int size = queue.size();
+            count++;
+            for(int i=0; i< size;i++){
+                
+                //System.out.println(queue);
+                String current = queue.poll();
+                if(current.equals(end))
+                    return count-1;
+                List<String> mutations = generateMutation(current);
+        
+                for(String mutation :mutations){
+                    if(geneBank.contains(mutation)){
+                        geneBank.remove(mutation);
+                        queue.add(mutation);
+                    }
+                }
+                
+            }
+        }
+        
+       return -1;   
+    }
+    
+    public List<String>  generateMutation(String geneString){
+        
+        List<String> mutations = new ArrayList();
+        List<Character> genes = new ArrayList();
+        genes.add('A');
+        genes.add('C');
+        genes.add('G');
+        genes.add('T');
+        
+        char[] charArray = geneString.toCharArray(); 
+        for(int i=0 ;i<geneString.length();i++){
+            char temp =  charArray[i];
+            for(char ch: genes){
+                charArray[i]= ch;
+                String newGeneString = String.valueOf(charArray);
+                
+                mutations.add(newGeneString);
+            }
+            charArray[i]= temp;
+        }
+        
+        return mutations;
+    }
+}

28d369f8153b9fae5a0f44a91258fd327fec9174
Lakshmi Tanuja Tammireddy <tanujatammireddy@gmail.com>
2022-02-07 12:29:41 -0600

    Time: 32 ms (30.16%), Space: 61.4 MB (15.03%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.java
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.java
@@ -1,36 +1,31 @@
 class Solution {
     public int maxDistance(int[][] grid) {
         
-        int water = 0;
-        int land = 1;
-        
         int n = grid.length;
         int maxDistance = Integer.MIN_VALUE;
         
         int[][] directions = {{0,1},{1,0},{0,-1},{-1,0}};
         boolean visited[][] = new boolean[n][n];
         int landCount = 0;
-        int waterCount = 0;
         
         Queue<int[]> queue = new LinkedList();
         
         for(int i=0; i<n; i++){
             for(int j=0; j<n; j++){
-                
-                if(grid[i][j] == land)
+                // Land
+                if(grid[i][j] == 1)
                 {
                     queue.add(new int[]{i,j});
                     landCount++;
                     visited[i][j] = true; 
                 }
-                else
-                    waterCount++;
                 
             }
         }
-        if(landCount == n*n || waterCount== n*n )
+        if(landCount == n*n || landCount == 0)
             return -1;
         
+        // Using BFS to calculate the distance to water, 
         while(!queue.isEmpty()){
             
             int[] current = queue.poll();

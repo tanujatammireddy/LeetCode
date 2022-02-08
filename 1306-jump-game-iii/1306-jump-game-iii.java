f3d530311099f6a1b4b225b37bf1c41a948ad058
Lakshmi Tanuja Tammireddy <tanujatammireddy@gmail.com>
2022-02-08 12:14:07 -0600

    Time: 4 ms (59.52%), Space: 61.9 MB (12.61%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.java
+++ 1306-jump-game-iii/1306-jump-game-iii.java
@@ -1,13 +1,13 @@
 class Solution {
     
-    boolean [] visited;
+    
     public boolean canReach(int[] arr, int start) {
         
-        visited = new boolean[arr.length];
-        return helper(arr,start);
+        boolean [] visited = new boolean[arr.length];
+        return helper(arr,start,visited);
     }
     
-    public boolean helper(int[] arr, int pos){
+    public boolean helper(int[] arr, int pos,boolean [] visited){
         
         if(pos >= 0 && pos < arr.length && !(visited[pos])){
             
@@ -17,8 +17,8 @@ class Solution {
         
             int new_pos_1 = pos + arr[pos];
             int new_pos_2 = pos - arr[pos]; 
-            boolean new_pos1 = helper(arr,new_pos_1);
-            boolean new_pos2 = helper(arr,new_pos_2);
+            boolean new_pos1 = helper(arr,new_pos_1,visited);
+            boolean new_pos2 = helper(arr,new_pos_2,visited);
     
             return new_pos2 || new_pos1;
         }

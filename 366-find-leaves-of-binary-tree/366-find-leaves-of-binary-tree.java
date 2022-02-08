c0590486d63c7e701dccb1ddb0a8b4e473eca1aa
Lakshmi Tanuja Tammireddy <tanujatammireddy@gmail.com>
2022-02-07 19:17:38 -0600

    Time: 0 ms (100.00%), Space: 42.6 MB (6.10%) - LeetHub

--- 366-find-leaves-of-binary-tree/366-find-leaves-of-binary-tree.java
+++ 366-find-leaves-of-binary-tree/366-find-leaves-of-binary-tree.java
@@ -14,44 +14,39 @@
  * }
  */
 class Solution {
+    
+    List<List<Integer>> leaves;
     public List<List<Integer>> findLeaves(TreeNode root) {
         
-        List<List<Integer>> leavesList = new ArrayList();
-        
+        leaves = new ArrayList();
         if(root != null)
-            helper(root,leavesList);
+            helper(root);
         
-        return leavesList;
-        
+        return leaves;
+            
     }
     
-    public int helper(TreeNode root,List<List<Integer>> leavesList){
+    public int helper(TreeNode root){
         
-        int depth = -1;
+        int depth ;
+        int left = 0;
+        int right = 0;
         if(root.left == null && root.right == null){
-            depth = 0;
+             depth = 0;
         }
-        else{
-            
-            int left_depth = 0;
-            int right_depth = 0;
-            if(root.left != null){
-                left_depth = helper(root.left,leavesList);
-            }
-            if(root.right != null){
-                right_depth = helper(root.right,leavesList);
-            }
-            
-            depth = Math.max(left_depth,right_depth) + 1;
-            
-        }
-        
-        if(leavesList.size() <= depth)
+        else
         {
-           leavesList.add(new ArrayList());
+            if(root.left != null)
+                left = helper(root.left);
+            if(root.right != null)
+                right = helper(root.right);
+            
+            depth = Math.max(left,right)+1;
         }
-        leavesList.get(depth).add(root.val);
-        return depth;  
+        if(leaves.size() == depth)
+            leaves.add(new ArrayList());
+        leaves.get(depth).add(root.val);
         
+        return depth;
     }
 }

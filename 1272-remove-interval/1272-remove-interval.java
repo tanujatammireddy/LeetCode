96ec91b97abc5070b2df69181c0934d04870d59b
Lakshmi Tanuja Tammireddy <tanujatammireddy@gmail.com>
2022-02-08 14:31:43 -0600

    Time: 6 ms (94.49%), Space: 50.7 MB (63.94%) - LeetHub

--- 1272-remove-interval/1272-remove-interval.java
+++ 1272-remove-interval/1272-remove-interval.java
@@ -5,40 +5,24 @@ class Solution {
         
         for(int[] interval: intervals){
             
-            
             int start = interval[0];
             int end = interval[1];
             List<Integer>list = new ArrayList();
             
-            
+            // Out of range to be removed:
             if(end < toBeRemoved[0]|| start > toBeRemoved[1]){
                 list.add(start);
                 list.add(end);
                 returnList.add(list);
                 continue;
             }
-            else if(start < toBeRemoved[0] && end > toBeRemoved[0]){
+            // start lies between the to be removed range
+            if(start < toBeRemoved[0] && end > toBeRemoved[0]){
                 list.add(start);
                 list.add(toBeRemoved[0]);
                 returnList.add(list);
-                
-                
             }
-            else if( toBeRemoved[0] < start && start < toBeRemoved[1] ){
-                
-               /* if(end < toBeRemoved[1]) 
-                    continue;
-                else
-                {
-                    //System.out.println("start"+start+" end"+end);
-                    list.add(start+1);
-                    //if
-                    list.add(end);
-                    returnList.add(list);
-                    
-                }*/
-                
-            }
+            // end lies between the to be removed range
             if(end >  toBeRemoved[1]){
                 List<Integer>sublist = new ArrayList();
                 sublist.add(toBeRemoved[1]);

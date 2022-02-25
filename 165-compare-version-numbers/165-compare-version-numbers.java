15e8ac746e890c3b3d953138ca3518254cd849bb
Lakshmi Tanuja Tammireddy <tanujatammireddy@gmail.com>
2022-02-25 15:54:24 -0600

    Time: 1 ms (90.92%), Space: 39.9 MB (49.30%) - LeetHub

--- 165-compare-version-numbers/165-compare-version-numbers.java
+++ 165-compare-version-numbers/165-compare-version-numbers.java
@@ -6,21 +6,18 @@ class Solution {
         
         int len = Math.max(v1.length,v2.length);
         
-        int len1 = v1.length;
-        int len2 = v2.length;
         int idx1 = 0;
         int idx2 = 0;
         
         
         while(len > 0){
             
-            
-            int ver1 = (idx1 < len1) ? Integer.valueOf(v1[idx1]) :0;
-            int ver2 = (idx2 < len2) ? Integer.valueOf(v2[idx2]) :0;
+            int ver1 = (idx1 < v1.length) ? Integer.valueOf(v1[idx1]) :0;
+            int ver2 = (idx2 < v2.length) ? Integer.valueOf(v2[idx2]) :0;
             len--;
             idx1++;
             idx2++;
-            //System.out.println(ver1+" "+ver2);
+            
             if(ver1 == ver2)
                 continue;
             else if(ver1 < ver2)

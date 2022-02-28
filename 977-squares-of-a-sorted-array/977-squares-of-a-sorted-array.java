d0b9919a6068d9fc39935b11a845d94afce70028
Lakshmi Tanuja Tammireddy <tanujatammireddy@gmail.com>
2022-02-27 21:01:15 -0600

    Time: 2 ms (82.02%), Space: 56 MB (14.05%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.java
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.java
@@ -26,13 +26,9 @@ class Solution {
             return result;
        
         }
-        else
-        {
-            for(int i=0;i< nums.length;i++){
+        for(int i=0;i< nums.length;i++){
                 nums[i] *= nums[i];
-            }
         }
-        
         return nums;
     }
 }

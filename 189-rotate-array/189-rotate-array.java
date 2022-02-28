d1442582317be1711dfeb3faabdba7e004375a01
Lakshmi Tanuja Tammireddy <tanujatammireddy@gmail.com>
2022-02-27 21:08:50 -0600

    Time: 2 ms (47.52%), Space: 64.9 MB (10.75%) - LeetHub

---
+++ 189-rotate-array/189-rotate-array.java
@@ -0,0 +1,27 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        
+        int len = nums.length;
+        if(k == len)
+            return;
+        if(k > len)
+            k = k%len;
+        len--;
+        reverse(0,len,nums);
+        reverse(0,k-1,nums);
+        reverse(k,len,nums);
+        
+    }
+    
+    public void reverse(int start, int end, int[] nums){
+        
+        while(start < end){
+            
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}

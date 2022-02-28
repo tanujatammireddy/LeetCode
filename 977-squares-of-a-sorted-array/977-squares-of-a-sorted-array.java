8fdeb9ef0e12eed8efc0adcb77a85f34864b15a8
Lakshmi Tanuja Tammireddy <tanujatammireddy@gmail.com>
2022-02-27 21:00:44 -0600

    Time: 2 ms (82.02%), Space: 55.3 MB (35.93%) - LeetHub

---
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+     
+        if(nums[0] < 0){
+            
+            int idx = nums.length-1;
+            int negativeIndex = 0;
+            int positiveIndex = nums.length-1;
+            int[] result = new int[nums.length];
+            
+            while(negativeIndex < positiveIndex){
+                
+                //System.out.println("negativeIndex:"+negativeIndex+"  positiveIndex:"+positiveIndex);
+                if(nums[positiveIndex] > Math.abs(nums[negativeIndex]) ){
+                    result[idx] = nums[positiveIndex]* nums[positiveIndex];
+                    positiveIndex--;
+                }
+                else{
+                    result[idx] = nums[negativeIndex]* nums[negativeIndex];
+                    negativeIndex++;
+                }
+                idx--;
+            }
+            if(negativeIndex == positiveIndex)
+                result[idx] = nums[negativeIndex]* nums[negativeIndex];
+            //System.out.println("negativeIndex:"+negativeIndex+"  positiveIndex:"+positiveIndex);
+            
+            return result;
+       
+        }
+        else
+        {
+            for(int i=0;i< nums.length;i++){
+                nums[i] *= nums[i];
+            }
+        }
+        
+        return nums;
+    }
+}

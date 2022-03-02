9cf27ace0eefbfab139f1cde66dc179bccd3ccfe
Lakshmi Tanuja Tammireddy <tanujatammireddy@gmail.com>
2022-03-01 22:53:48 -0600

    Time: 1 ms (99.97%), Space: 48.5 MB (16.39%) - LeetHub

--- 338-counting-bits/338-counting-bits.java
+++ 338-counting-bits/338-counting-bits.java
@@ -3,18 +3,19 @@ class Solution {
         
         int[] result = new int[n+1];
         result[0] = 0;
-        int two_power = 2;
-        
+        int twoPower = 2;
+        int previousTwoPower = 2;
         if(n > 0){
             result[1] = 1;
         
             for(int i= 2; i<= n;i++){
-                if(i== two_power){
+                if(i== twoPower){
                     result[i] = 1;
-                    two_power *= 2;
+                    previousTwoPower = twoPower;
+                    twoPower *= 2;
                 }
                 else
-                    result[i] =  1 + result[i-two_power/2];
+                    result[i] =  1 + result[i-previousTwoPower];
             
         }
         }

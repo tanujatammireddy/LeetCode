827dd07134a604fa80a732d8893e750cf193e1a7
Lakshmi Tanuja Tammireddy <tanujatammireddy@gmail.com>
2022-03-01 23:02:07 -0600

    Time: 2 ms (81.34%), Space: 235 MB (17.28%) - LeetHub

---
+++ 1490-clone-n-ary-tree/1490-clone-n-ary-tree.java
@@ -0,0 +1,46 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    
+    public Node() {
+        children = new ArrayList<Node>();
+    }
+    
+    public Node(int _val) {
+        val = _val;
+        children = new ArrayList<Node>();
+    }
+    
+    public Node(int _val,ArrayList<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    public Node cloneTree(Node root) {
+        
+        if(root != null){
+            return helper(root);
+        }
+        
+        return root;
+        
+        
+    }
+    
+    public Node helper(Node root){
+        
+        Node clone = new Node(root.val);
+        for(Node child: root.children){
+            clone.children.add(helper(child));
+        }
+        
+        return clone;
+        
+    }
+}

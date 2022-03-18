21a7c1a5ec933d03ce1b301a950d6d772e4efdec
Lakshmi Tanuja Tammireddy <tanujatammireddy@gmail.com>
2022-03-18 18:45:06 -0500

    Time: 1 ms (68.94%), Space: 42 MB (41.29%) - LeetHub

---
+++ 856-score-of-parentheses/856-score-of-parentheses.java
@@ -0,0 +1,26 @@
+class Solution { 
+    
+    public int scoreOfParentheses(String s) {
+        
+        Stack<Integer> stack =  new Stack<Integer>();
+        stack.push(0);
+        int idx = 0;
+        
+        while(idx < s.length()){
+            
+            char c = s.charAt(idx);
+            if(c == '('){
+                stack.push(0);
+            }   
+            else{
+                stack.push(Math.max(2*stack.pop(),1)+stack.pop());
+                
+            }   
+            idx++;
+            
+        }
+        
+        return stack.pop();
+        
+    }
+}

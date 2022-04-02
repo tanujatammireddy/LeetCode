d280cf005c58245becc2a2c2e1e50fc61f0836c3
Lakshmi Tanuja Tammireddy <tanujatammireddy@gmail.com>
2022-04-02 01:27:47 -0500

    Time: 25 ms (40.51%), Space: 98.6 MB (67.09%) - LeetHub

--- 1775-equal-sum-arrays-with-minimum-number-of-operations/1775-equal-sum-arrays-with-minimum-number-of-operations.java
+++ 1775-equal-sum-arrays-with-minimum-number-of-operations/1775-equal-sum-arrays-with-minimum-number-of-operations.java
@@ -3,7 +3,6 @@ class Solution {
         
         int result = 0;
         int sum1 = 0;
-        
         for(int num:nums1)
             sum1 += num;
         
@@ -14,6 +13,7 @@ class Solution {
         Arrays.sort(nums1);
         Arrays.sort(nums2);
         
+        /* Passing the array with heighest sum as Array1*/
         if(sum1 > sum2)
             result = helper(nums1, nums2, sum1, sum2);
         else if(sum1 < sum2)
@@ -23,11 +23,14 @@ class Solution {
         
     }
     
+    /*Using greedy approach and pointers reducing the sum of the array with a larger sum or increasing the array's sum with the smaller sum*/
+    
     public int helper(int[] nums1, int[] nums2, int sum1, int sum2){
         
         int max_pointer = nums1.length -1;
         int min_pointer = 0;
         
+        //**Parsing the lowest sum array from starting to add points and Height sum array from the end to remove the points**//
         int operations_count = 0;
         
         while((max_pointer >= 0 || min_pointer < nums2.length) && sum1 != sum2){
@@ -37,26 +40,25 @@ class Solution {
             int valueToAdd = 0;
             int valueToRemove = 0;
             
-            if(min_pointer < nums2.length && sum1 - sum2 > 0 && nums2[min_pointer] < 6)
+            /*Calculating the points we can add*/
+            if(min_pointer < nums2.length && diff > 0 && nums2[min_pointer] < 6)
             {
-                diff = sum1 - sum2;
                 valueToAdd = (6 - nums2[min_pointer]) ;
                 valueToAdd = (valueToAdd > diff) ? diff: valueToAdd;
-                
             }
-            if(max_pointer >= 0 && sum1 - sum2 > 0 && nums1[max_pointer] > 1)
+            /*Calculating the points we can reduce*/
+            if(max_pointer >= 0 && diff > 0 && nums1[max_pointer] > 1)
             {
-                diff = sum1 - sum2;
                 valueToRemove = (nums1[max_pointer]-1);
                 valueToRemove = (valueToRemove > diff) ? diff : valueToRemove;
                     
             }
-            
+            /*If we can not do any further operations break condition*/
             if(valueToAdd == 0 && valueToRemove == 0)
                 break;
             
+            /*Deciding to add or remove values based on the heighest value*/
             if(valueToAdd > valueToRemove){
-                
                 sum2 += valueToAdd;
                 nums2[min_pointer] += valueToAdd ;
                 operations_count++;
@@ -69,10 +71,10 @@ class Solution {
                 operations_count++;
                 max_pointer--;
                 
-            }
-            
+            } 
         }
         
+        //verify if the idle condition is met
         return (sum1 - sum2 <= 0 )?operations_count:-1;
         
     }

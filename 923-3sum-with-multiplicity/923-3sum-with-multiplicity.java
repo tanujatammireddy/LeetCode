30e841b7bc0e52ce9d73b133cf9d4bc983555d84
Lakshmi Tanuja Tammireddy <tanujatammireddy@gmail.com>
2022-04-05 21:51:38 -0500

    Time: 54 ms (43.15%), Space: 44.6 MB (38.31%) - LeetHub

--- 923-3sum-with-multiplicity/923-3sum-with-multiplicity.java
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.java
@@ -1,42 +1,40 @@
 class Solution {
     public int threeSumMulti(int[] arr, int target) {
         
-        int mod = 1_000_000_007;
+        int mod = 1000000007;
         long count = 0;
-        
         Arrays.sort(arr);
-        
         for(int i=0;i< arr.length;i++){
             
-            int sum = target-arr[i];
             int j = i+1;
-            int k = arr.length-1;
+            int k = arr.length -1;
             
+            int sum = target - arr[i];
             while(j< k){
                 
                 if(arr[j] + arr[k] < sum)
                     j++;
-                else if(arr[j] +arr[k] > sum)
+                else if(arr[j] + arr[k] > sum)
                     k--;
-                
                 else if(arr[j] != arr[k]){
+                    
                     int left = 1;
                     int right = 1;
                     
-                    while(j+1 < k && arr[j] == arr[j+1] ){
+                    while(j+1<k && arr[j] == arr[j+1]){
                         left++;
                         j++;
                     }
-                    
-                    while(k-1 > j && arr[k] == arr[k-1]){
+                    while( k-1 > j && arr[k-1] == arr[k]){
                         right++;
                         k--;
                     }
                     
-                    count += (left * right);
+                    count += (left*right);
                     count %= mod;
                     j++;
                     k--;
+                    
                 }
                 else{
                     count += (k-j+1)*(k-j)/2;
@@ -44,9 +42,12 @@ class Solution {
                     break;
                 }
                 
+                
+                
             }
         }
         
         return (int)count;
+        
     }
 }

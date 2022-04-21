3aa3c238560331abcb6a1e8a239a9e42c543ef5d
Lakshmi Tanuja Tammireddy <tanujatammireddy@gmail.com>
2022-04-20 20:15:43 -0500

    Time: 3 ms (28.54%), Space: 41.7 MB (24.80%) - LeetHub

---
+++ 69-sqrtx/69-sqrtx.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int mySqrt(int x) {
+        
+        if(x <= 1)
+            return x;
+        int low = 2;
+        int high = x/2;
+        int mid = 0;
+        while(low <= high){
+            
+            mid = (low)+(high-low)/2;
+             
+            long val = (long) mid*mid;
+            
+            if( val == x)
+                return mid;
+            
+            if(val > x)
+                high = mid-1;
+            else
+                low = mid+1;
+            
+        }
+        //System.out.println("low"+low+" high"+high+" mid"+mid);
+            
+        return low-1;
+    }
+}

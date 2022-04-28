7c356392947a777f231c91c71ee5f9064118d858
Lakshmi Tanuja Tammireddy <tanujatammireddy@gmail.com>
2022-04-28 14:52:31 -0500

    Time: 10 ms (39.64%), Space: 74.8 MB (9.61%) - LeetHub

--- 1151-minimum-swaps-to-group-all-1s-together/1151-minimum-swaps-to-group-all-1s-together.java
+++ 1151-minimum-swaps-to-group-all-1s-together/1151-minimum-swaps-to-group-all-1s-together.java
@@ -1,49 +1,41 @@
 class Solution {
-    public int minSwaps(int[] data) {
-        
-        if(data.length > 1){
-            
-            int len = data.length;
-            int totalOnes = 0;
-            for(int num:data){
-                if(num == 1)
-                    totalOnes++;
-            }
-            
-            if(totalOnes == 1) return 0;
-        
-            int left = 0;
-            int right = 0;
-            int zerosCount = 0;
-            int minZeros = Integer.MAX_VALUE ;
-            
-            while(right < totalOnes){
-                if(data[right] == 0)
-                    zerosCount++;
-                right++;  
-            }
-            
-            minZeros = Math.min(minZeros,zerosCount); 
-            //System.out.println(""+zerosCount);
-            while(right < len){
-               
-                if(data[left] == 0){
-                    zerosCount--;
-                }
-                if(data[right] == 0){
-                    zerosCount++;
-                }
-                
-                left++;
-                right++;
-               minZeros = Math.min(minZeros,zerosCount);   
-            }
-            
-            return minZeros;
-            
+public int minSwaps(int[] data) {
+
+    int len = data.length;
+    int totalOnes = 0;
+    for(int num:data){
+        if(num == 1)
+            totalOnes++;
         }
-        return 0;
-            
-        
+
+    if(totalOnes == 1) return 0;
+
+    int left = 0;
+    int right = 0;
+    int zerosCount = 0;
+    int minZeros = Integer.MAX_VALUE ;
+
+    while(right < totalOnes){
+        if(data[right] == 0)
+            zerosCount++;
+        right++;  
     }
+
+    minZeros = Math.min(minZeros,zerosCount); 
+
+    while(right < len){
+
+        if(data[left] == 0)
+            zerosCount--;
+        if(data[right] == 0)
+            zerosCount++;
+        left++;
+        right++;
+        
+        minZeros = Math.min(minZeros,zerosCount);   
+        }
+
+    return minZeros;
+
+}
 }

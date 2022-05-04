6c65b8bafe3ce4290493927fd3aa0d42944a15dd
Lakshmi Tanuja Tammireddy <tanujatammireddy@gmail.com>
2022-05-03 22:43:30 -0500

    Time: 1 ms (91.39%), Space: 45.7 MB (6.38%) - LeetHub

--- 1629-slowest-key/1629-slowest-key.java
+++ 1629-slowest-key/1629-slowest-key.java
@@ -2,23 +2,26 @@ class Solution {
     public char slowestKey(int[] releaseTimes, String keysPressed) {
         
         TreeMap<Integer,List<Character>> map = new TreeMap();
+        
         char[] charArray = keysPressed.toCharArray();
-        map.put(releaseTimes[0],new ArrayList<Character>());
-        map.get(releaseTimes[0]).add(charArray[0]);
+        int maxTime = releaseTimes[0];
+        char ch = charArray[0];
         
         for(int i=1; i< releaseTimes.length;i++){
             
-            int time = releaseTimes[i] -releaseTimes[i-1];
-            if(!map.containsKey(time))
-                 map.put(time,new ArrayList<Character>());
-            map.get(time).add(charArray[i]);
+            int time = releaseTimes[i]- releaseTimes[i-1];
+            
+            if(time > maxTime){
+                maxTime = time;
+                ch = charArray[i];
+            }
+            else if(time == maxTime){
+                ch = (ch > charArray[i] )? ch:charArray[i];
+            }
             
         }
         
-        List<Character> list = map.get(map.lastKey()); 
-        if(list.size() >1)
-            Collections.sort(list);
-        
-        return list.get(list.size()-1);
+       
+        return ch;
     }
 }

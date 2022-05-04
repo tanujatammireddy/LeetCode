7d3ee4833ad1f322ab4860f070e472164941a53d
Lakshmi Tanuja Tammireddy <tanujatammireddy@gmail.com>
2022-05-03 21:30:11 -0500

    Time: 7 ms (41.00%), Space: 45.1 MB (34.74%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.java
@@ -0,0 +1,30 @@
+class Solution {
+    List<List<Integer>> result;
+    int tar;
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        
+        result = new ArrayList();
+        tar = target;
+        helper(candidates,0,0,new ArrayList());
+        return result;
+    }
+    public void helper(int[] candidates, int pos, int sum,List<Integer> list){
+        
+        if(sum > tar)
+            return;
+        if(sum == tar){
+            result.add(new ArrayList(list));
+        }
+        else
+        {
+            for(int i= pos;i< candidates.length;i++){
+                list.add(candidates[i]);
+                helper(candidates,i,sum+candidates[i],list);
+                list.remove(list.size()-1);
+                
+            }
+            
+        }
+    }
+    
+}

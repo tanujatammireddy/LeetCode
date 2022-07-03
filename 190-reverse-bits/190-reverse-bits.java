ad609d2a6d5c7e507018159afcf0fbe20f03a0e0
Lakshmi Tanuja Tammireddy <tanujatammireddy@gmail.com>
2022-07-03 14:48:29 -0500

    Time: 1 ms (98.63%), Space: 41.5 MB (92.13%) - LeetHub

---
+++ 190-reverse-bits/190-reverse-bits.java
@@ -0,0 +1,21 @@
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        
+        int reverse = 0;
+        int count = 0;
+        
+        while(count++ <32){
+            
+            reverse = reverse << 1;
+            
+            if(n != 0){
+                reverse = (n&1) | reverse;
+                n = n >> 1;
+            }
+            
+        }
+        return reverse;
+        
+    }
+}

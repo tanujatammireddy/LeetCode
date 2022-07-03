5642acac5a84b0c3ce132d4f8ffb8567af274363
Lakshmi Tanuja Tammireddy <tanujatammireddy@gmail.com>
2022-07-03 14:51:32 -0500

    Time: 2 ms (29.00%), Space: 42 MB (79.22%) - LeetHub

--- 190-reverse-bits/190-reverse-bits.java
+++ 190-reverse-bits/190-reverse-bits.java
@@ -1,26 +1,26 @@
 public class Solution {
-    // you need treat n as an unsigned value
-    public int reverseBits(int n) {
-        
-        int reverse = 0;
-        int count = 0;
-        
-        while(count++ <32){
-            
-            // left shift on reverse to add new digits
-            reverse = reverse << 1;
-            
-            if(n != 0){
-                // (n & 1) checks if there is 1 in the end of n 
-                // '|' added digit to the reverse 
-                
-                reverse = (n&1) | reverse;
-                // right shift on input to get the digit to be added.
-                n = n >> 1;
-            }
-            
+// you need treat n as an unsigned value
+public int reverseBits(int n) {
+
+    int reverse = 0;
+    int count = 0;
+
+    // As the number given is 32 bit we iterate the same process for all the 32 bits 
+    while(count++ <32){
+
+        // left shift on reverse to add new digits
+        reverse = reverse << 1;
+
+        if(n != 0){
+            // (n & 1) checks if there is 1 in the end of n 
+            // '|' added digit to the reverse 
+            reverse = (n&1) | reverse;
+            // right shift on input to get the digit to be added.
+            n = n >> 1;
         }
-        return reverse;
-        
+
     }
+    return reverse;
+
+}
 }

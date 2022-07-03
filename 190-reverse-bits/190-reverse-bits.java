22127b4d53043bd673c8117476846198366d2d56
Lakshmi Tanuja Tammireddy <tanujatammireddy@gmail.com>
2022-07-03 14:50:50 -0500

    Time: 1 ms (98.63%), Space: 43 MB (15.25%) - LeetHub

--- 190-reverse-bits/190-reverse-bits.java
+++ 190-reverse-bits/190-reverse-bits.java
@@ -7,10 +7,15 @@ public class Solution {
         
         while(count++ <32){
             
+            // left shift on reverse to add new digits
             reverse = reverse << 1;
             
             if(n != 0){
+                // (n & 1) checks if there is 1 in the end of n 
+                // '|' added digit to the reverse 
+                
                 reverse = (n&1) | reverse;
+                // right shift on input to get the digit to be added.
                 n = n >> 1;
             }
             
